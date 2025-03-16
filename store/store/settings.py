97eb753c0a52d215608878f37a0849015d7caf97
denis <academic666@mail.ru>
2025-03-16 13:00:45 +0100

    Fix db building

--- store/store/settings.py
+++ store/store/settings.py
@@ -17,8 +17,13 @@ import rest_framework.pagination
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-DATABASE_DIR = '/store/database'
 
+IN_DOCKER = os.getenv("IN_DOCKER", "0") == "1"
+if IN_DOCKER:
+    DATABASE_DIR = '/store/database'
+else:
+    DATABASE_DIR = BASE_DIR / 'database'
+    DATABASE_DIR.mkdir(exist_ok=True)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
