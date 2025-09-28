7ff5a44b8cf1b60d3e016c13a8f1c6c2be7dff34
Denis Pisarev <dzianisngu@gmail.com>
2025-09-28 22:37:42 +0200

    Update settings.py
    change static/media dirs depend on DOCKER

--- store/store/settings.py
+++ store/store/settings.py
@@ -142,11 +142,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/store/static'
-
-MEDIA_ROOT = '/store/media'
 
 MEDIA_URL = '/media/'
+
+if IN_DOCKER:
+    STATIC_ROOT = '/store/static'
+    MEDIA_ROOT = '/store/media'
+else:
+    STATIC_ROOT = BASE_DIR / 'static'
+    MEDIA_ROOT = BASE_DIR / 'media'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
